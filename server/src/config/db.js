b890a156416253e2c7ea9732818d0ca533b50e29
krparduman2021 <YOUR_GITHUB_EMAIL>
2026-07-05 22:28:25 +0530

    enclave portal server code

---
+++ server/src/config/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import logger from "../utils/logger.js";
+
+const connectDB = async () => {
+  try {
+    console.log(process.env.MONGO_URI)
+    const connection = await mongoose.connect(process.env.MONGO_URI);
+
+    logger.info(
+      `MongoDB Connected : ${connection.connection.host}`
+    );
+  } catch (error) {
+    logger.error(`MongoDB Connection Failed : ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
